6e0bb3fba61d7364fdb17489e3373d601b10178a
Neal Stublen <neal@treefroglabs.com>
2016-08-28 22:32:21 -0500

    Add "dice-roller" solution.

--- dice-roller/js/dice-roller.js
+++ dice-roller/js/dice-roller.js
@@ -12,6 +12,63 @@
  */
 
 function initializeDiceRoller() {
+    function clearDiceImageElements() {
+        var $rolledDice = document.getElementsByClassName("dice");
+        while ($rolledDice.length > 0) {
+            $rolledDice[0].remove();
+        }
+    }
+
+    function createDiceImageElement(number) {
+        var $img = document.createElement("img");
+        $img.className = "dice";
+        $img.src = "./images/dice-" + number + ".svg";
+        return $img;
+    }
+
+    function createRandomDiceImageElement() {
+        var randomNumber = Math.floor(Math.random() * 6) + 1;
+        return createDiceImageElement(randomNumber);
+    }
+
+    function rollDice() {
+        var $diceCount = document.getElementById("dice-count");
+        console.log("Roll " + $diceCount.value + " dice");
+        rollDiceCount($diceCount.value);
+    }
+
+    function rollDiceCount(count) {
+        clearDiceImageElements();
+
+        var $rolledDice = document.getElementById("rolled-dice");
+
+        for (var index = 0; index < count; ++index) {
+            var $die = createRandomDiceImageElement();
+            $rolledDice.appendChild($die);
+        }
+    }
+
+    function rollDiceWithAnimation() {
+        rollDice();
+
+        var $diceAnimation = document.getElementById("dice-animation");
+        if ($diceAnimation.checked) {
+            var spin = 10;
+            var timerId = setInterval(function () {
+                rollDice();
+                spin--;
+                if (spin <= 0) {
+                    clearInterval(timerId);
+                }
+            }, 100)
+        }
+    }
+
+    var $form = document.getElementsByTagName("form")[0];
+    $form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        rollDiceWithAnimation();
+    });
 }
 
 window.addEventListener("load", function () {
