77266f5317349e52077867443e58c7c8ff00c38d
Neal Stublen <neal@treefroglabs.com>
2016-08-30 22:46:32 -0500

    Add "keypad" solution.

--- keypad/js/keypad.js
+++ keypad/js/keypad.js
@@ -10,6 +10,33 @@
 
 function initializeKeypad() {
 
+    // Append a single digit to the current contents of the
+    // entry field.
+    function appendDigit(digit) {
+        var $entry = document.getElementById("keypad-entry");
+        $entry.innerText += digit;
+    }
+
+    // Event callback function that will respond to a key
+    // click by appending the digit within the key to the
+    // entry field.
+    function appendDigitFromKey(event) {
+        appendDigit(event.target.innerText);
+    }
+
+    // Listen for click events on the keypad keys.
+    var $keys = document.getElementsByClassName("keypad-key");
+    for (var index = 0; index < $keys.length; ++index) {
+        var $key = $keys[index];
+        $key.addEventListener("click", appendDigitFromKey);
+    }
+
+    // Listen for a click event on the Clear button.
+    var $clear = document.getElementById("clear");
+    $clear.addEventListener("click", function () {
+        var $entry = document.getElementById("keypad-entry");
+        $entry.innerText = "";
+    });
 }
 
 window.addEventListener("load", initializeKeypad);
